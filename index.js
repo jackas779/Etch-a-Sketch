b04489f282786dbcf836440322ffda4df26b4102
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-09-12 20:28:05 -0500

    add functions from all  buttons and styles

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    cuadricula('');
+    cuadricula(''); pintar('negro');
 });
 
 function cuadricula (valor){
-    let pintar = false;
     // llama al div que contendra la cuadricula
     const cl16 = document.querySelector('.container');
 
@@ -23,19 +22,8 @@ function cuadricula (valor){
         contenDiv[i]= oneDiv;
 
     }
-    // verifica cual es el div que hay que pintar
-    function fHover(e){
-        if(pintar){
-            e.target.style.backgroundColor = 'black';
-        }
-    }
     // agrega divs al contendedor
     contenDiv.forEach((e) => cl16.appendChild(e));
-
-    // enventos del div
-    cl16.addEventListener('mousedown',() => pintar = true);
-    cl16.addEventListener('mouseup',() => pintar = false);
-    cl16.addEventListener('mousemove',fHover);
 };
 
 // funcion para cammbiar la escala de la cudricula la cuadricula
@@ -67,3 +55,49 @@ function tamano(){
 }
 
 
+// funcion para pintar la cuadricula para el color negro y el alcoiris y el borrador
+
+function pintar(val){
+    /// incializamos en falso para que no marque por que si
+    let pintar = false;
+
+    // verifica cual es el div que hay que pintar
+    function fHover(e){
+        if(pintar && val == 'negro'){
+            e.target.style.backgroundColor = 'black';
+        }
+        else if(pintar && val == 'ramdon'){
+            e.target.style.backgroundColor = generarNuevoColor();
+        }
+        else if(pintar && val == 'borrador'){
+            e.target.style.backgroundColor = 'bisque';
+        }
+    }
+
+    // llama al div que contendra la cuadricula
+    const cl16 = document.querySelector('.container');
+
+    // enventos del div
+    cl16.addEventListener('mousedown',() => pintar = true);
+    cl16.addEventListener('mouseup',() => pintar = false);
+    cl16.addEventListener('mousemove',fHover);
+}
+
+/// funcion para crear colores. 
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+    return color;
+}
+
+/// funcion para limpiar la cuadricula sin volver a cargarla
+
+function erasedAll(){
+    const container = document.querySelector('.container');
+    container.childNodes.forEach((e)=>e.style.backgroundColor='bisque');
+}
